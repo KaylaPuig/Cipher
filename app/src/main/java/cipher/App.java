0d3fd0868588768a255bb06dbcb1b1ea4677c745
Kayla <kayla.puig@gmail.com>
2023-01-05 23:39:51 -0500

    Split program into encrypt/decrypt functions

--- app/src/main/java/cipher/App.java
+++ app/src/main/java/cipher/App.java
@@ -7,24 +7,7 @@ public class App {
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Enter a random seed: ");
-        String seedStr = myScanner.nextLine();
-
-        /* Turn this seed into 
-         * (1) a base shift amount like a regular Caesar Cipher, and
-         * (2) an amount for the phrase to shift after each letter
-        */
-        int shiftNum = 0;
-        int seed = 0;
-        boolean tempB = true;
-        for(char c : seedStr.toCharArray()) {
-            if(tempB)
-                seed += c;
-            else 
-                shiftNum += c;
-            tempB = !tempB;
-        }
-        seed = seed % 26;
-        shiftNum = 1 + shiftNum % 25;
+        String key = myScanner.nextLine();
 
         System.out.println();
         System.out.println("Enter your phrase to encrypt/decrypt");
@@ -34,7 +17,32 @@ public class App {
         System.out.println("Do you want to encrypt or decrypt? (E/d)");
         boolean doEncrypt = !myScanner.nextLine().toLowerCase().equals("d");
 
+        String result = doEncrypt ? encrypt(phrase, key) : decrypt(phrase, key);
+
+        System.out.println();
+        System.out.println("Your result is \"" + result + "\"");
+        myScanner.close();
+    }
+
+    public static String encrypt(String phrase, String key) {
+        int shiftNum = 0;
+        int seed = 0;
         String result = "";
+        boolean tempB = true;
+
+        /* Turn key into
+         * (1) a base shift amount like a regular Caesar Cipher, and
+         * (2) an amount for the phrase to shift after each letter
+        */
+        for(char c : key.toCharArray()) {
+            if(tempB)
+                seed += c;
+            else 
+                shiftNum += c;
+            tempB = !tempB;
+        }
+        seed = seed % 26;
+        shiftNum = 1 + shiftNum % 25;
 
         int i = 0;
         for(char c : phrase.toCharArray()) {
@@ -46,17 +54,49 @@ public class App {
             char base = (Character.isUpperCase(c)) ? 'A' : 'a';
 
             int temp = c - base;
-            if(doEncrypt)
-                temp = Math.floorMod(temp + seed + (shiftNum * i), 26);
-            else // Decrypt
-                temp = Math.floorMod(temp - seed - (shiftNum * i), 26);
+            temp = Math.floorMod(temp + seed + (shiftNum * i), 26);
             result += Character.toString(base + temp);
 
             i++;
         }
+        return result;
+    }
 
-        System.out.println();
-        System.out.println("Your result is \"" + result + "\"");
-        myScanner.close();
+    public static String decrypt(String phrase, String key) {
+        int shiftNum = 0;
+        int seed = 0;
+        String result = "";
+        boolean tempB = true;
+
+        /* Turn key into
+         * (1) a base shift amount like a regular Caesar Cipher, and
+         * (2) an amount for the phrase to shift after each letter
+        */
+        for(char c : key.toCharArray()) {
+            if(tempB)
+                seed += c;
+            else 
+                shiftNum += c;
+            tempB = !tempB;
+        }
+        seed = seed % 26;
+        shiftNum = 1 + shiftNum % 25;
+
+        int i = 0;
+        for(char c : phrase.toCharArray()) {
+            if(!Character.isAlphabetic(c)) {
+                result += c;
+                continue;
+            }
+
+            char base = (Character.isUpperCase(c)) ? 'A' : 'a';
+
+            int temp = c - base;
+            temp = Math.floorMod(temp - seed - (shiftNum * i), 26);
+            result += Character.toString(base + temp);
+
+            i++;
+        }
+        return result;
     }
 }
