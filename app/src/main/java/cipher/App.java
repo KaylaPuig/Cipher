2eec71d726b2ca0254a60cef8aeb9e8a78b33a8b
Kayla <kayla.puig@gmail.com>
2023-01-05 23:51:31 -0500

    Expanded cipher to include all alphanumeric characters and symbols
    Also split the functions and cipher characters into a separate class with static members

--- app/src/main/java/cipher/App.java
+++ app/src/main/java/cipher/App.java
@@ -17,86 +17,10 @@ public class App {
         System.out.println("Do you want to encrypt or decrypt? (E/d)");
         boolean doEncrypt = !myScanner.nextLine().toLowerCase().equals("d");
 
-        String result = doEncrypt ? encrypt(phrase, key) : decrypt(phrase, key);
+        String result = doEncrypt ? Cipher.encrypt(phrase, key) : Cipher.decrypt(phrase, key);
 
         System.out.println();
         System.out.println("Your result is \"" + result + "\"");
         myScanner.close();
-    }
-
-    public static String encrypt(String phrase, String key) {
-        int shiftNum = 0;
-        int seed = 0;
-        String result = "";
-        boolean tempB = true;
-
-        /* Turn key into
-         * (1) a base shift amount like a regular Caesar Cipher, and
-         * (2) an amount for the phrase to shift after each letter
-        */
-        for(char c : key.toCharArray()) {
-            if(tempB)
-                seed += c;
-            else 
-                shiftNum += c;
-            tempB = !tempB;
-        }
-        seed = seed % 26;
-        shiftNum = 1 + shiftNum % 25;
-
-        int i = 0;
-        for(char c : phrase.toCharArray()) {
-            if(!Character.isAlphabetic(c)) {
-                result += c;
-                continue;
-            }
-
-            char base = (Character.isUpperCase(c)) ? 'A' : 'a';
-
-            int temp = c - base;
-            temp = Math.floorMod(temp + seed + (shiftNum * i), 26);
-            result += Character.toString(base + temp);
-
-            i++;
-        }
-        return result;
-    }
-
-    public static String decrypt(String phrase, String key) {
-        int shiftNum = 0;
-        int seed = 0;
-        String result = "";
-        boolean tempB = true;
-
-        /* Turn key into
-         * (1) a base shift amount like a regular Caesar Cipher, and
-         * (2) an amount for the phrase to shift after each letter
-        */
-        for(char c : key.toCharArray()) {
-            if(tempB)
-                seed += c;
-            else 
-                shiftNum += c;
-            tempB = !tempB;
-        }
-        seed = seed % 26;
-        shiftNum = 1 + shiftNum % 25;
-
-        int i = 0;
-        for(char c : phrase.toCharArray()) {
-            if(!Character.isAlphabetic(c)) {
-                result += c;
-                continue;
-            }
-
-            char base = (Character.isUpperCase(c)) ? 'A' : 'a';
-
-            int temp = c - base;
-            temp = Math.floorMod(temp - seed - (shiftNum * i), 26);
-            result += Character.toString(base + temp);
-
-            i++;
-        }
-        return result;
     }
 }
---
+++ app/src/main/java/cipher/Cipher.java
@@ -0,0 +1,74 @@
+package cipher;
+
+public class Cipher {
+    public static final String characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`-=[]\\;',./~!@#$%^&*()_+{}|:\"<>?";
+
+    public static String encrypt(String phrase, String key) {
+        int shiftNum = 0;
+        int seed = 0;
+        String result = "";
+        boolean tempB = true;
+
+        /* Turn key into
+         * (1) a base shift amount like a regular Caesar Cipher, and
+         * (2) an amount for the phrase to shift after each letter
+        */
+        for(char c : key.toCharArray()) {
+            if(tempB)
+                seed += c;
+            else 
+                shiftNum += c;
+            tempB = !tempB;
+        }
+        seed = seed % 26;
+        shiftNum = 1 + shiftNum % 25;
+
+        int i = 0;
+        for(char c : phrase.toCharArray()) {
+            if(!characters.contains(Character.toString(c))) {
+                result += c;
+                continue;
+            }
+
+            int index = characters.indexOf(Character.toString(c));
+            result += Character.toString(characters.charAt(Math.floorMod(index + seed + (shiftNum * i), characters.length())));
+            i++;
+        }
+        return result;
+    }
+
+    public static String decrypt(String phrase, String key) {
+        int shiftNum = 0;
+        int seed = 0;
+        String result = "";
+        boolean tempB = true;
+
+        /* Turn key into
+         * (1) a base shift amount like a regular Caesar Cipher, and
+         * (2) an amount for the phrase to shift after each letter
+        */
+        for(char c : key.toCharArray()) {
+            if(tempB)
+                seed += c;
+            else 
+                shiftNum += c;
+            tempB = !tempB;
+        }
+        seed = seed % 26;
+        shiftNum = 1 + shiftNum % 25;
+
+        int i = 0;
+        for(char c : phrase.toCharArray()) {
+            if(!characters.contains(Character.toString(c))) {
+                result += c;
+                continue;
+            }
+
+            int index = characters.indexOf(Character.toString(c));
+            result += Character.toString(characters.charAt(Math.floorMod(index - seed - (shiftNum * i), characters.length())));
+            i++;
+        }
+        return result;
+    }
+}
