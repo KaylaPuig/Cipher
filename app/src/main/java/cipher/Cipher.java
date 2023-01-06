2e4566de914fc3ec25b605f94b09a3930d345a83
Kayla <kayla.puig@gmail.com>
2023-01-06 00:24:38 -0500

    Created a different seed system

--- app/src/main/java/cipher/Cipher.java
+++ app/src/main/java/cipher/Cipher.java
@@ -4,25 +4,20 @@ public class Cipher {
     public static final String characters =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`-=[]\\;',./~!@#$%^&*()_+{}|:\"<>?";
 
-    public static String encrypt(String phrase, String key) {
-        int shiftNum = 0;
+    public static int generateSeed(String key) {
         int seed = 0;
-        String result = "";
-        boolean tempB = true;
-
-        /* Turn key into
-         * (1) a base shift amount like a regular Caesar Cipher, and
-         * (2) an amount for the phrase to shift after each letter
-        */
-        for(char c : key.toCharArray()) {
-            if(tempB)
-                seed += c;
-            else 
-                shiftNum += c;
-            tempB = !tempB;
+        
+        for(int i = 0; i < key.length(); i++) {
+            int character = (int) key.charAt(i);
+            seed += character << ((8 * i) % 32);
         }
-        seed = seed % characters.length();
-        shiftNum = 1 + shiftNum % (characters.length() - 1);
+
+        return seed;
+    }
+
+    public static String encrypt(String phrase, String key) {
+        int seed = generateSeed(key);
+        String result = "";
 
         int i = 0;
         for(char c : phrase.toCharArray()) {
@@ -32,31 +27,15 @@ public class Cipher {
             }
 
             int index = characters.indexOf(Character.toString(c));
-            result += Character.toString(characters.charAt(Math.floorMod(index + seed + (shiftNum * i), characters.length())));
+            result += Character.toString(characters.charAt(Math.floorMod(index + seed + (seed * i), characters.length())));
             i++;
         }
         return result;
     }
 
     public static String decrypt(String phrase, String key) {
-        int shiftNum = 0;
-        int seed = 0;
+        int seed = generateSeed(key);
         String result = "";
-        boolean tempB = true;
-
-        /* Turn key into
-         * (1) a base shift amount like a regular Caesar Cipher, and
-         * (2) an amount for the phrase to shift after each letter
-        */
-        for(char c : key.toCharArray()) {
-            if(tempB)
-                seed += c;
-            else 
-                shiftNum += c;
-            tempB = !tempB;
-        }
-        seed = seed % characters.length();
-        shiftNum = 1 + shiftNum % (characters.length() - 1);
 
         int i = 0;
         for(char c : phrase.toCharArray()) {
@@ -66,7 +45,7 @@ public class Cipher {
             }
 
             int index = characters.indexOf(Character.toString(c));
-            result += Character.toString(characters.charAt(Math.floorMod(index - seed - (shiftNum * i), characters.length())));
+            result += Character.toString(characters.charAt(Math.floorMod(index - seed - (seed * i), characters.length())));
             i++;
         }
         return result;
